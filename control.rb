f4167eaa0f895199add2c9b26a54d77a29d55d4a
Eric Benoit <benwa02@gmail.com>
2014-09-26 10:33:36 -0400

    Cleaner, leaner.

--- control.rb
+++ control.rb
@@ -1,85 +1,51 @@
 
-puts "===================="
+puts "=============================="
 puts "Welcome to Ruby Weather Center"
 puts "Written by Eric Benoit"
-puts "===================="
+puts "=============================="
 puts
 
 puts "What is the temperature today?"
-inputted_temperature_f = gets.chomp
-inputted_temperature_f = inputted_temperature_f.to_i
+TEMPERATURE_F_INPUT = gets.chomp.to_i
+# Needs error handling for checking user input is actually an integer.
+puts "\r"
 
-puts "Would you like weather in Celsius? Type 'n' for NO or 'y' for YES. \n"
-user_wants_celsius = gets.chomp
+puts "Would you like the temperature in Celsius?\nType 'y' for Celsius or hit any key for Farenheit.\n"
+celsius_user_input = gets.chomp
+puts "\r"
 
-if user_wants_celsius == "y"
-	celsius_flag = true
-	degrees_as = "C"
-	todays_temperature = (inputted_temperature_f - 32) * 1.8
+if celsius_user_input == "yes" || celsius_user_input == "y" || celsius_user_input == "Y"
+	CELSIUS_FLAG = true
+	measurement = "C"
 else
-	celsius_flag = false
-	degrees_as = "F"
-	todays_temperature = inputted_temperature_f
+	CELSIUS_FLAG = false
+	measurement = "F"
 end
 
-response = "Hello, the temperature is #{todays_temperature}°#{degrees_as} \n" + "Our thoughts:"
+if CELSIUS_FLAG == true
+	todays_temperature = (TEMPERATURE_F_INPUT - 32) * 1.8
+else
+	todays_temperature = TEMPERATURE_F_INPUT
+end
 
-case inputted_temperature_f
-when 0..14
-	puts response << "its cold, your frozen, boom"
+response = "Hello, the temperature is #{todays_temperature.round}°#{measurement} \n" + "Our thoughts: "
+
+case TEMPERATURE_F_INPUT
+when -99..14
+	# Not ideal for any temperature less than 0.
+	puts response << "It's damn cold, your frozen, boom!"
 when 15..30
-	puts response << "holy fucking a it's cold"
+	puts response << "Holy batman it is cold!"
 when 31..45
-	puts response << "still cold but walkable to a bar"
+	puts response << "Still cold but walkable to a bar."
 when 46..60
-	puts response << "time to go swimming... right"
+	puts response << "Time to go swimming... right."
 when 61..75
-	puts response << "jeans and tshirt, heck yeah"
+	puts response << "Jeans and tshirt, heck yeah!"
 when 76..90
-	puts response << "whoa there buddy"
+	puts response << "Whoa there buddy!"
 else
-	puts response << "beach mohn"
+	puts response << "Get to the beach mohn!"
 end
 
-
-# celsius_flag = false
-
-# puts "What is the temperature today?"
-# user_temperature = gets.chomp
-
-# if celsius_flag == true
-# 	todays_temperature_c = user_temperature / -17.2222
-# 	degrees_as = "°C"
-# else
-# 	todays_temperature_f = user_temperature
-# 	degrees_as = "°F"
-# end
-
-# response = "Hello, the temperature is #{user_temperature}#{degrees_as} \n" + "Our thoughts:"
-
-# todays_temperature_f = user_temperature.to_i
-# todays_temperature_c = todays_temperature_f.to_i / -17.2222
-
-# if celsius_flag == false
-# 	response = "Hello, the temperature is #{todays_temperature_f}°F \n" + "Our thoughts:"
-# else
-# 	response = "Hello, the temperature is #{todays_temperature_c.round}°C \n" + "Our thoughts:"
-# end
-
-# if todays_temperature_f < 0
-# 	puts "holy fuck it's cold"
-# elsif todays_temperature_f < 15
-# 	puts "still cold but walkable to a bar"
-# elsif todays_temperature_f < 30
-# 	puts "pleasantly cold"
-# elsif todays_temperature_f < 45
-# 	puts "scarf and beanie"
-# elsif todays_temperature_f < 60
-# 	puts "time to go swimming... right"
-# elsif todays_temperature_f < 75
-# 	puts "jeans and tshirt, heck yeah"
-# elsif todays_temperature_f < 90
-# 	puts "whoa there buddy"
-# else todays_temperature_f >= 90
-# 	puts "beach mohn"
-# end
+puts "\r"
